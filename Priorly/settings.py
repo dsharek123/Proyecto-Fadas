5cd098742f71b1af7e7d453d7fcd0548318e6109
m4rulx <lepinamaru@gmail.com>
2025-10-07 20:49:29 -0300

    Agregue la app calendario, la integre  las settings, y vincule la urls de la app con las urls generales

--- Priorly/settings.py
+++ Priorly/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'cuentas',
     'apuntes',
     'flashcard',
+    'calendario',
 ]
 
 MIDDLEWARE = [
--- Priorly/urls.py
+++ Priorly/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cuentas.urls')),
     path('apuntes/',include('apuntes.urls')),
+    path('calendario/',include('calendario.urls')),
 ]
