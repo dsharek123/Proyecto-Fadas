759fa5c32a26814c0daff9bc7e66bc3d2d70fbdb
m4rulx <lepinamaru@gmail.com>
2025-10-25 21:21:54 -0300

    Agregue la estructura delcalendario, con las funciones principales, me falta agregarle es css y otras opciones

--- calendario/models.py
+++ calendario/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tema(models.Model):
+    tema = models.CharField(max_length=200)
+    actividad = models.CharField(max_length=200, blank=True)
+    fecha = models.DateField(null=True, blank=True)
+    def __str__(self):
+        return self.tema
--- calendario/urls.py
+++ calendario/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import vista_calendario
+from .views import vista_calendario, nuevo_tema, editar_tema, eliminar_tema
 
 urlpatterns = [
     path('', vista_calendario, name='vista_calendario'),
+    path('nuevo_tema/', nuevo_tema, name='nuevo_tema'),
+    path('editar_tema/<int:tema_id>/', editar_tema, name='editar_tema'),
+    path('eliminar_tema/<int:tema_id>/', eliminar_tema, name='eliminar_tema'),
 ]
