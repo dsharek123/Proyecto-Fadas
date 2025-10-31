b7eada790e8a5d2d915c8dc544764687f00e7da6
hacsic <amatam1212@gmail.com>
2025-10-30 21:49:15 -0300

    Apartado de ver apuntes y elminar, arreglado el inicio de sesión

--- apuntes/urls.py
+++ apuntes/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('crear/', views.crear_apunte, name='crear_apunte'),
     path('seleccion/', views.seleccion_apuntes, name='seleccion_apuntes'),
+    path('creados/',views.apuntes_creados, name='apuntes_creados'),
+    path('eliminar/<int:apunte_id>/', views.eliminar_apunte, name='eliminar_apunte'),
+    path('detalle/<int:apunte_id>/', views.detalle_apunte, name='detalle_apunte'),
 ]
